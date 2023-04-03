32ff45dd4fb9e570eee2a4da8d220b28c876f2d7
Eldiley  Teles (Ley) <eldileyamado@gmail.com>
2023-04-04 00:29:57 +0100

    modificando script

--- script.js
+++ script.js
@@ -68,6 +68,7 @@ function preload ()
     this.load.image('car5','assets/car5.png');
     this.load.image('car6','assets/car6.png');
     this.load.image('car7','assets/car7.png');
+    this.load.image('vida','assets/vida.png');
 }
 
 
@@ -83,7 +84,7 @@ function create ()
     platforms.create(20, 250, 'ground');
     platforms.create(780, 220, 'ground');
     textoVidas = this.add.text(16, 16, 'Vidas: ' + vidas, { fontSize: '32px', fill: '#fff' });
-    textoPontos = this.add.text(game.config.width / 2 - 100, 16, 'Pontos: ' + pontos, { fontSize: '32px', fill: '#fff' });
+    textoPontos = this.add.text(game.config.width / 2 - 100, 16, 'Pontos: 0', { fontSize: '32px', fill: '#fff' });
     textoNivel = this.add.text(600, 16, 'Nível: ' + level, { fontSize: '32px', fill: '#fff' });
     textoGameOver = this.add.text(200, 250, 'GAME OVAR!', { fontSize: '64px', fill: '#fff' });
     textoGameOver.visible = false;
@@ -111,12 +112,21 @@ function create ()
 
     this.physics.add.collider(player, platforms);
     player.canJump = true;
+    
+    // adicionar objetos no grupo
+    grupovida = this.physics.add.group();
     grupostar = this.physics.add.group();
     grupostar2 = this.physics.add.group();
     grupo1 = this.physics.add.group();
     grupo2 = this.physics.add.group();
     grupo3 = this.physics.add.group();
 
+    this.time.addEvent({ 
+        delay: 1000,
+        callback: createFallingObjectvida,
+        callbackScope: this, 
+        loop: true 
+    });
     this.time.addEvent({ 
         delay: 1000,
         callback: createFallingObjectstar,
@@ -148,8 +158,9 @@ function create ()
         callbackScope: this, 
         loop: true 
     });
-
-    // adicionar objetos no grupo
+    
+    
+    
 }
 
 function update ()
@@ -157,14 +168,14 @@ function update ()
     if (cursors.left.isDown)
     {
         player.flipX = true // fazer movimento de
-        player.setVelocityX(-200);
+        player.setVelocityX(-300);
 
       
     }// funcao para fazer movimento para direita
     else if (cursors.right.isDown)
     {
         player.flipX = false // fazer movimento
-        player.setVelocityX(200);
+        player.setVelocityX(300);
 
     }
     else
@@ -191,7 +202,36 @@ function update ()
         return;
     }
 
+    if (mKey.isDown) {
+        lives = 100;
+        textoVidas.setText('Vidas: ' + lives);
+    }
+
+    this.physics.add.collider(grupovida,platforms);
+    this.physics.add.collider(grupostar,platforms);
+    this.physics.add.collider(grupostar2,platforms);
+    this.physics.add.collider(grupo1,platforms);
+    this.physics.add.collider(grupo2,platforms);
+    this.physics.add.collider(grupo3,platforms);
+    this.physics.add.overlap(player,grupostar,collectstar,null,this);
+    this.physics.add.overlap(player,grupostar2,collectstar2,null,this);
+    this.physics.add.overlap(player,grupovida,collectstavida,null,this);
     
+}
+function collectstavida(player,grupovida){
+    grupovida.destroy();
+    vidas += 1;
+    textoVidas.setText('Vidas: ' + vidas);
+}
+function collectstar(player,grupostar){
+    grupostar.destroy();
+    pontos += 1;
+    textoPontos.setText('Pontos: ' + pontos);
+}
+function collectstar2(player,grupostar2){
+    grupostar2.destroy();
+    pontos += 1;
+    textoPontos.setText('Pontos: ' + pontos);
 }
 function createFallingObjectstar2() {
     var x = Phaser.Math.Between(90,700, game.config.width);
@@ -228,10 +268,18 @@ function createFallingObject2() {
 function createFallingObject3() {
     var x = Phaser.Math.Between(90,750, game.config.width);
     var y = -50;
-    var car3 = this.physics.add.sprite(x, y, 'car4');
+    var car3 = this.physics.add.sprite(x, y, 'car5');
     grupo3.add(car3);
     car3.setGravityY(150);
     car3.setBounce(0.7);
+}
+function createFallingObjectvida() {
+    var x = Phaser.Math.Between(90,750, game.config.width);
+    var y = -50;
+    var vida = this.physics.add.sprite(x, y, 'vida');
+    grupovida.add(vida);
+    vida.setGravityY(100);
+    vida.setBounce(0.5);
 }
 
 
